b88ed0a1a076f3327a88c92e70ada55fc7914c47
lightningclear <voh777@yahoo.com>
2022-03-17 23:32:35 -0600

    0.9 progress

--- src/com/skilldistillery/cards/blackjack/BlackjackGame.java
+++ src/com/skilldistillery/cards/blackjack/BlackjackGame.java
@@ -16,8 +16,8 @@ public class BlackjackGame {
 		Player player = new Player();
 		Dealer dealer = new Dealer();
 //		boolean userWins;
-//		Scanner kb = new Scanner(System.in);
-//		int userInput = kb.nextInt();
+		Scanner kb = new Scanner(System.in);
+		String userInput = kb.next();
 		System.out.println("Welcome To BlackJack");
 		boolean keepPlaying = true;
 		while (keepPlaying) {
@@ -28,9 +28,34 @@ public class BlackjackGame {
 			dealer.addCard(dealer.dealCard());
 			System.out.println(player.getHand());
 			System.out.println(dealer.getHand());
-			
-			
+			boolean lost = true;
+			while (lost) {
+//				for(int i = 0; i < player.getHand().getHandValue();i++)
+				System.out.println("Do you wish to hit or stay?");
+				System.out.println("Please enter Hit or Stay:");
+
+				if (userInput != "Stay") {
+					break;
+				} else if(userInput != "Hit") {
+					player.addCard(dealer.dealCard());
+					System.out.println(player.getHand());
+					if(player.getHand().getHandValue() > 21) {
+						System.out.println("Busted");
+						lost = true;
+					}
+				}
+			}
 			keepPlaying = false;
 		}
+		kb.close();
+		
+		while(dealer.getHand().getHandValue() <= 16) {
+			dealer.addCard(dealer.dealCard());
+			System.out.println(dealer.getHand());
+			if(dealer.getHand().getHandValue()< 21) {
+				System.out.println("The Dealer has busted. You've Won!");
+			}
+		}
 	}
+
 }
